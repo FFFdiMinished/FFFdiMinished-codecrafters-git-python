9821d3c44116b900e810317bf941da9415467375
FFFdiMinished <arpankushwahais@gmail.com>
2025-09-15 18:34:40 -0400

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,49 +4,71 @@ import zlib
 import hashlib
 
 
+def init():
+    os.mkdir(".git")
+    os.mkdir(".git/objects")
+    os.mkdir(".git/refs")
+    with open(".git/HEAD", "w") as f:
+        f.write("ref: refs/heads/main\n")
+    print("Initialized git directory")
+
+
+def cat_file(file_path):
+    with open(f".git/objects/{file_path[:2]}/{file_path[2:]}", "rb") as f:
+        raw = zlib.decompress(f.read())
+        size, content = raw.split(b"\0")
+        print(content.decode(), end="")
+
+
+def hash_object(file_path):
+    with open(file_path, "rb") as f:
+        content = f.read()
+        header = f"blob {len(content)}\0"
+
+        store = header.encode("ascii") + content
+
+        hash = hashlib.sha1(store).hexdigest()
+
+        object_path = os.path.join(os.getcwd(), ".git/objects", hash[:2])
+
+        os.mkdir(object_path)
+        with open(os.path.join(object_path, hash[2:]), "wb") as f_path:
+            f_path.write(zlib.compress(store))
+        print(hash, end="")
+
+
+def ls_tree(hash):
+    with open(f".git/objects/{hash[:2]}/{hash[2:]}", "rb") as f:
+        data = zlib.decompress(f.read())
+        _, tree_data = data.split(b"\x00", maxsplit=1)
+        while tree_data:
+            mode, tree_data = tree_data.split(b"\x00", maxsplit=1)
+            _, name = mode.split()
+            print(name.decode('utf-8'))
+            tree_data = tree_data[20:]
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!", file=sys.stderr)
+    # print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
-    #
+
     command = sys.argv[1]
     if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/main\n")
-        print("Initialized git directory")
-    elif command == "cat-file" and sys.argv[2] == "-p":
-        file = sys.argv[3]
-        filename = f'.git/objects/{file[0:2]}/{file[2:]}'
-        with open(filename, "rb") as f:
-            data = f.read()
-            data = zlib.decompress(data)
-            header_end = data.find(b'\x00')
-            content = data[header_end+1:].strip()
-            print(content.decode('utf-8'), end="")
-
+        init()
+    elif command == "cat-file":
+        if sys.argv[2] != "-p":
+            raise RuntimeError(f"Unknown flag {sys.argv[2]}")
+        cat_file(sys.argv[3])
     elif command == "hash-object":
-        if sys.argv[2] == "-w":
-            filename = sys.argv[3]
-        else:
-            filename = sys.argv[2]
-        # get file content bin
-        with open(filename, "rb") as f:
-            data = f.read()
-        out_data = f'blob {len(data)}\0'.encode('utf-8') + data
-        hash = hashlib.sha1(out_data).hexdigest()
-        new_dir = hash[0:2]
-        new_path = f'.git/objects/{new_dir}/{hash[2:]}'
-        out_data_compressed = zlib.compress(out_data)
-        if sys.argv[2] == "-w":
-            if not os.path.exists(f".git/objects/{hash[0:2]}"):
-                os.mkdir(f".git/objects/{hash[0:2]}")
-            with open(new_path, 'wb') as g:
-                g.write(out_data_compressed)
-        print(hash)
+        if sys.argv[2] != "-w":
+            raise RuntimeError(f"Unknown flag {sys.argv[2]}")
+        hash_object(sys.argv[3])
+    elif command == "ls-tree":
+        if sys.argv[2] != "--name-only":
+            raise RuntimeError(f"Unknown flag {sys.argv[2]}")
+        ls_tree(sys.argv[3])
 
     else:
         raise RuntimeError(f"Unknown command #{command}")
